e44c6af8b0262cabd511cfea4b2cdc84e99a53b5
Anna Kosheleva <AWIkrKos@yandex.ru>
2023-02-15 21:21:50 +0300

    add 3-1

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from .views import CreateProject, UpdateProject, MeasurementProject, ListProject, RetrieveProject
 
 urlpatterns = [
+    path('create/', CreateProject.as_view()),
+    path('update/', UpdateProject.as_view()),
+    path('measurement/', MeasurementProject.as_view()),
+    path('list/', ListProject.as_view()),
+    path('retrieve/<pk>/', RetrieveProject.as_view())
+
     # TODO: зарегистрируйте необходимые маршруты
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,32 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from rest_framework import generics
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
+
+
+
+def CreateProject(request):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class UpdateProject(generics.UpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class MeasurementProject(generics.ListAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+
+class ListProject(generics.ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class RetrieveProject(generics.RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
