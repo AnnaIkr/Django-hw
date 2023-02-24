b4cce3cd37b1f073a6c2590c72df04ab75f19f2d
Anna Kosheleva <AWIkrKos@yandex.ru>
2023-02-24 20:47:53 +0300

    3-3 permissions

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from rest_framework.exceptions import ValidationError
 from advertisements.models import Advertisement
 
 
@@ -40,6 +40,7 @@ class AdvertisementSerializer(serializers.ModelSerializer):
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
-        # TODO: добавьте требуемую валидацию
+        if Advertisement.objects.all().filter(status="OPEN", creator_id=self.context["request"].user).count() > 10:
+            raise ValidationError('Ads limit is over!')
 
         return data
